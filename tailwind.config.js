6e966b7bc09d8d40939ecdd341fd4ece1499b2ca
amarkalsait <158206083+amarkalsait@users.noreply.github.com>
2024-07-05 22:05:22 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custom-bg': "url('/media/bgimage.jpg')",
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
